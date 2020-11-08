de538602273929ddc6cc9c1f210b80bd4d44040a
Pierre Miniggio <pierre.miniggio@gmail.com>
2020-11-08 18:05:00 +0100

    add token

--- src/index.js
+++ src/index.js
@@ -1,36 +1,46 @@
-import {createMachine, interpret, state, transition} from 'robot3'
+import {createMachine, interpret, reduce, state, transition} from 'robot3'
 
-const machine = createMachine({
-  loggedOut: state(
-    transition('register', 'register'),
-    transition('login', 'login')
-  ),
-  loggedIn: state(
-    transition('loggedOut', 'loggedOut'),
-    transition('findGame', 'findGame')
-  ),
-  register: state(
-    transition('loggedOut', 'loggedOut'),
-    transition('loggedIn', 'loggedIn')
-  ),
-  login: state(
-    transition('loggedOut', 'loggedOut'),
-    transition('loggedIn', 'loggedIn')
-  ),
-  findGame: state(
-    transition('loggedIn', 'loggedIn'),
-    transition('loggedOut', 'loggedOut'),
-    transition('game', 'game')
-  ),
-  game: state(
-    transition('findGame', 'findGame'),
-    transition('loggedOut', 'loggedOut')
-  )
-})
+const machine = createMachine(
+  'loggedOut',
+  {
+    loggedOut: state(
+      transition('register', 'registerForm'),
+      transition('login', 'loginForm')
+    ),
+    loggedIn: state(
+      transition('addToken', 'loggedIn', reduce((ctx, ev) => ({...ctx, token: ev.value}))),
+      transition('logout', 'loggedOut'),
+      transition('findGame', 'findGame')
+    ),
+    registerForm: state(
+      transition('logout', 'loggedOut'),
+      transition('loggedIn', 'loggedIn')
+    ),
+    loginForm: state(
+      transition('logout', 'loggedOut'),
+      transition('loggedIn', 'loggedIn')
+    ),
+    findGame: state(
+      transition('loggedIn', 'loggedIn'),
+      transition('logout', 'loggedOut'),
+      transition('game', 'game')
+    ),
+    game: state(
+      transition('findGame', 'findGame'),
+      transition('logout', 'loggedOut')
+    )
+  },
+  () => ({token: null})
+)
 
 const service = interpret(machine, () => {
-
+  //console.log('Etat : ' + service.machine.current)
+  console.log('Contexte : ' + JSON.stringify(service.context))
 })
 console.log(service.machine.current)
-service.send('register')
+service.send('login')
+console.log(service.machine.current)
+service.send('loggedIn')
+console.log(service.machine.current)
+service.send({type: 'addToken', value: 'blablabla'})
 console.log(service.machine.current)
