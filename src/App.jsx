1ed6462c588196aa7608d4fbba4bedbfe8f1b7f1
Pierre Miniggio <pierre.miniggio@gmail.com>
2020-11-08 19:14:45 +0100

    can

--- src/App.jsx
+++ src/App.jsx
@@ -4,33 +4,34 @@ import { useMachine } from './Navigation/useMachine'
 import machine from './Navigation/machine'
 
 export default function App() {
-  const [state, send] = useMachine(machine)
+  const [state, send, can] = useMachine(machine)
 
   return <Box p={2}>
     <h1>Fencer Game</h1>
     {state === 'loginForm' && <p>Login form</p>}
     {state === 'registerForm' && <p>Register form</p>}
-    <Button
+    {can('login') && <Button
       variant="contained"
       color="primary"
       onClick={() => send('login')}
     >
       Login
-    </Button>
-    <Button
+    </Button>}
+    {can('register') && <Button
       variant="contained"
       color="primary"
       onClick={() => send('register')}
     >
       Register
-    </Button>
-    <Button
+    </Button>}
+
+    {can('home') && <Button
       variant="contained"
       color="primary"
       onClick={() => send('home')}
     >
       Home
-    </Button>
+    </Button>}
     {state}
   </Box>
 }
--- src/Navigation/useMachine.js
+++ src/Navigation/useMachine.js
@@ -3,19 +3,36 @@ import { interpret } from "robot3";
 
 /**
  * @param {import("robot3").Machine} machine 
+ * 
+ * // TODO type return
  */
 export function useMachine(machine) {
     const {current: service} = useRef(interpret(machine, () => {
         setState(service.machine.current)
     }))
     const [state, setState] = useState(service.machine.current)
-    /**
-     * @param {string} type 
-     * @param {Object} params 
-     */
-    const send = useCallback(function(type, params = {}) {
-        service.send({type: type, ...params})
-    }, [service])
+    
+    const send = useCallback(
+        /**
+         * @param {string} type 
+         * @param {Object} params 
+         */
+        function(type, params = {}) {
+            service.send({type: type, ...params})
+        }, [service]
+    )
 
-    return [state, send]
+    const can = useCallback(
+        /**
+         * @param {string} transitionName
+         * 
+         * @returns {boolean}
+         */
+        (transitionName) => {
+            return service.machine.state.value.transitions.has(transitionName)
+        },
+        [service.machine.state.value.transitions]
+    )
+
+    return [state, send, can]
 }
