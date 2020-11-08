d4515088be8f2b7f19f71fcc1121093fa55ae91b
Pierre Miniggio <pierre.miniggio@gmail.com>
2020-11-08 17:32:20 +0100

    Root States

--- src/index.js
+++ src/index.js
@@ -1,11 +1,31 @@
 import {createMachine, interpret, state, transition} from 'robot3'
 
 const machine = createMachine({
-  home: state(transition('register', 'register')),
-  register: state(),
-  login: state(),
-  findGame: state(),
-  game: state()
+  loggedOut: state(
+    transition('register', 'register'),
+    transition('login', 'login')
+  ),
+  loggedIn: state(
+    transition('loggedOut', 'loggedOut'),
+    transition('findGame', 'findGame')
+  ),
+  register: state(
+    transition('loggedOut', 'loggedOut'),
+    transition('loggedIn', 'loggedIn')
+  ),
+  login: state(
+    transition('loggedOut', 'loggedOut'),
+    transition('loggedIn', 'loggedIn')
+  ),
+  findGame: state(
+    transition('loggedIn', 'loggedIn'),
+    transition('loggedOut', 'loggedOut'),
+    transition('game', 'game')
+  ),
+  game: state(
+    transition('findGame', 'findGame'),
+    transition('loggedOut', 'loggedOut')
+  )
 })
 
 const service = interpret(machine, () => {
