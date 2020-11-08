cb6f0e333d25888f9e04c1a0764cbb80497a47a8
Pierre Miniggio <pierre.miniggio@gmail.com>
2020-11-08 18:24:36 +0100

    App.jsx

--- src/App.js
+++
@@ -1,8 +0,0 @@
-import {createMachine, state} from 'robot3'
-
-const machine = createMachine({
-  register: state(),
-  login: state(),
-  findGame: state(),
-  game: state()
-})
---
+++ src/App.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import Box from '@material-ui/core/Box'
+
+export default function App() {
+  return <Box p={2}></Box>
+}
