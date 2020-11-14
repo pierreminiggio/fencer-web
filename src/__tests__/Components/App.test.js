098ee58fa2ebb56e97433f8397ac11eb0caf76d1
Pierre Miniggio <pierre.miniggio@gmail.com>
2020-11-14 14:35:09 +0100

    Move test

--- src/__tests__/Components/App.test.js
+++ src/__tests__/Components/App.test.js
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react'
-import App from '../../src/Components/App'
+import App from '../../Components/App'
 
 test('Home Page', () => {
     render(<App />)
