81d7d5e3f5a40d4c176a9755c532ad18bee76920
Pierre Miniggio <pierre.miniggio@gmail.com>
2020-11-14 14:40:41 +0100

    4 spaces

--- src/Domain/Auth/register.js
+++ src/Domain/Auth/register.js
@@ -7,41 +7,41 @@ function wait(ms) {
 export default createMachine(
     'form',
     {
-      form: state(
-        transition('input', 'form', reduce((ctx, ev) => {
-            const newCtx = {...ctx}
-            newCtx[ev.field] = ev.value
+        form: state(
+            transition('input', 'form', reduce((ctx, ev) => {
+                const newCtx = {...ctx}
+                newCtx[ev.field] = ev.value
 
-            return newCtx
-        })),
-        transition(
-            'submit',
-            'loading',
-            guard(ctx =>
-              ctx.login !== null &&
-              ctx.login !== '' &&
-              ctx.password !== null &&
-              ctx.password !== '' &&
-              ctx.confirm !== null &&
-              ctx.confirm !== ''
+                return newCtx
+            })),
+            transition(
+                'submit',
+                'loading',
+                guard(ctx =>
+                    ctx.login !== null &&
+                    ctx.login !== '' &&
+                    ctx.password !== null &&
+                    ctx.password !== '' &&
+                    ctx.confirm !== null &&
+                    ctx.confirm !== ''
+                )
             )
-        )
-      ),
-      loading: invoke(
-        () => wait(2000),
-        transition(
-            'done',
-            'success',
-            reduce((ctx, ev) => ({...ctx, token: ev.data.token}))
         ),
-        transition(
-            'error',
-            'edit',
-            reduce((ctx, ev) => ({...ctx, error: ev.error.message}))
-        )
-      ),
-      success: state(),
-      error: state()
-    },
-    () => ({login: null, password: null, confirm: null, token: null})
+        loading: invoke(
+            () => wait(2000),
+            transition(
+                'done',
+                'success',
+                reduce((ctx, ev) => ({...ctx, token: ev.data.token}))
+            ),
+            transition(
+                'error',
+                'edit',
+                reduce((ctx, ev) => ({...ctx, error: ev.error.message}))
+            )
+        ),
+        success: state(),
+        error: state()
+      },
+      () => ({login: null, password: null, confirm: null, token: null})
 )
