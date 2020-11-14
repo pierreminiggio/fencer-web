f40e75377e3d7f95db7001794128fb892c0ff022
Pierre Miniggio <pierre.miniggio@gmail.com>
2020-11-14 14:41:33 +0100

    4 spaces

--- src/Domain/navigation.js
+++ src/Domain/navigation.js
@@ -3,33 +3,33 @@ import {createMachine, guard, reduce, state, transition} from 'robot3'
 export default createMachine(
     'loggedOut',
     {
-      loggedOut: state(
-        transition('register', 'registerForm'),
-        transition('login', 'loginForm')
-      ),
-      loggedIn: state(
-        transition('logout', 'loggedOut'),
-        transition('findGame', 'findGame')
-      ),
-      registerForm: state(
-        transition('home', 'loggedOut'),
-        transition('loggedIn', 'loggedIn', guard(ctx => ctx.token !== null)),
-        transition('addToken', 'registerForm', reduce((ctx, ev) => ({...ctx, token: ev.value})))
-      ),
-      loginForm: state(
-        transition('home', 'loggedOut'),
-        transition('loggedIn', 'loggedIn', guard(ctx => ctx.token !== null)),
-        transition('addToken', 'loginForm', reduce((ctx, ev) => ({...ctx, token: ev.value})))
-      ),
-      findGame: state(
-        transition('loggedIn', 'loggedIn'),
-        transition('logout', 'loggedOut'),
-        transition('game', 'game')
-      ),
-      game: state(
-        transition('findGame', 'findGame'),
-        transition('logout', 'loggedOut')
-      )
+        loggedOut: state(
+            transition('register', 'registerForm'),
+            transition('login', 'loginForm')
+        ),
+        loggedIn: state(
+            transition('logout', 'loggedOut'),
+            transition('findGame', 'findGame')
+        ),
+        registerForm: state(
+            transition('home', 'loggedOut'),
+            transition('loggedIn', 'loggedIn', guard(ctx => ctx.token !== null)),
+            transition('addToken', 'registerForm', reduce((ctx, ev) => ({...ctx, token: ev.value})))
+        ),
+        loginForm: state(
+            transition('home', 'loggedOut'),
+            transition('loggedIn', 'loggedIn', guard(ctx => ctx.token !== null)),
+            transition('addToken', 'loginForm', reduce((ctx, ev) => ({...ctx, token: ev.value})))
+        ),
+        findGame: state(
+            transition('loggedIn', 'loggedIn'),
+            transition('logout', 'loggedOut'),
+            transition('game', 'game')
+        ),
+        game: state(
+            transition('findGame', 'findGame'),
+            transition('logout', 'loggedOut')
+        )
     },
     () => ({token: null})
 )
